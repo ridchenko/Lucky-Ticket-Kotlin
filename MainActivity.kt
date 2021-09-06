1a8a2fef1a733bf2709c78c8deae0c69bedb6f2d
ridchenko <88441052+ridchenko@users.noreply.github.com>
2021-09-06 15:28:26 +0300

    Edits 01

---
+++ MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.luckyticketkotlin
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val MAX_NUMBER_OF_DIGITS = 6
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun CheckTicket(view: View) {
+
+        val num = findViewById<EditText>(R.id.etTicketNumber).text.toString()
+        val picture = findViewById<ImageView>(R.id.imageView)
+        val pictureGreen = getDrawable(R.drawable.green_light)
+        val pictureRed = getDrawable(R.drawable.red_light)
+
+        if (num.length == MAX_NUMBER_OF_DIGITS) {
+
+                val result = num.toCharArray().map { it.toString().toInt() }
+
+                if (result[0] + result[1] + result[2] == result[3] + result[4] + result[5]) {
+                    picture.setImageDrawable(pictureGreen)
+                } else {
+                    picture.setImageDrawable(pictureRed)
+                }
+
+        } else {
+            Toast.makeText(applicationContext,"Enter 6 digits", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+}
